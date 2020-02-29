688d3b0a9a48081910f8fb491b7d9348bef7fa1f
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-03-01 01:32:17 +0530

    DP PROBLEM

---
+++ Codeforces/LIS.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lis(int* arr,int* dp,int n){
+    for(int i=2;i<=n;i++){
+        int best=0;
+        for(int j=1;j<=i;j++){
+            if(arr[i]>arr[j]){
+                best=max(best,dp[j]+1);
+            }
+        }
+        dp[i]=best;
+    }
+    int m=-1;
+    for(int i=1;i<=n;i++){
+        cout<<dp[i]<<" ";
+        if(dp[i]>m){
+            m=dp[i];
+        }
+    }
+    return m;
+}
+int main() {
+    int n;
+    cin>>n;
+    int* arr=new int[n+2];
+    for(int i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+    int * dp=new int[n+2];
+    for(int i=0;i<=n;i++){
+        dp[i]=1;
+    }
+    cout<<lis(arr,dp,n)<<endl;
+}
