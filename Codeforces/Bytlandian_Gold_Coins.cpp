7bb07fac22ced02d4084fac6a51ca12b9b137337
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-03-15 22:39:45 +0530

    Create Bytlandian_Gold_Coins.cpp

---
+++ Codeforces/Bytlandian_Gold_Coins.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+map<long int ,long int> mp;
+long int maxi(long int n){
+    if(n<=0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    if(mp.count(n)){
+        return mp[n];
+    }
+    long int a=maxi(n/2);
+    long int b=maxi(n/3);
+    long int c=maxi(n/4);
+    //cout<<"a: "<<a<<"  b: "<<b<<" c: "<<c<<endl;
+    mp[n]=max(n,a+b+c);
+    //cout<<n<<"  --->   "<<mp[n]<<"   "<<endl;
+    return mp[n];
+}
+int main() {
+    long int n;cin>>n;
+    cout<<maxi(n);
+}
